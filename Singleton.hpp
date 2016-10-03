071a4ff46d0cc21411189b493c82bf861ea1457a
Martin Chuckeles <martin@jakubik.info>
2016-10-03 10:33:09 +0200

    Add Singleton

---
+++ Singleton.hpp
@@ -0,0 +1,41 @@
+#include <memory>
+
+
+/**
+ * Singleton. Popular pattern. Inherit from this in singleton classes and define
+ * using SINGLETON_DEFINE macro.
+ */
+
+
+template<typename Type>
+class Singleton {
+
+public:
+
+    typedef std::shared_ptr<Type> Ptr;
+
+
+    static Ptr &GetSingleton() {
+        if (!msSingleton) {
+            msSingleton.reset(new Type());
+        }
+
+        return msSingleton;
+    }
+
+    Singleton(const Singleton<Type> &other) = delete;
+    void operator=(const Singleton<Type> &other) = delete;
+
+
+protected:
+
+    Singleton() = default;
+
+    static std::shared_ptr<Type> msSingleton;
+
+};
+
+/**
+ * Singleton definition help macro.
+ */
+#define SINGLETON_DEFINE(Type) Singleton<Type>::Ptr Singleton<Type>::msSingleton
