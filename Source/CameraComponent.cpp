6b90c6075e023fc576f28354b702f511496a70e9
Martin Chuckeles <martin@jakubik.info>
2016-10-24 00:01:47 +0200

    Make the camera listen to Update event and update view matrix

--- Source/CameraComponent.cpp
+++ Source/CameraComponent.cpp
@@ -5,20 +5,37 @@
 
 #include "Window.hpp"
 #include "ShaderProgram.hpp"
+#include "Entity.hpp"
+#include "TransformComponent.hpp"
 
 
 void CameraComponent::OnAttach(std::weak_ptr<Entity> entity) {
     Component::OnAttach(entity);
+    mEntity = entity;
 
     int width, height;
     Window::GetSingleton()->GetSize(width, height);
 
     auto projection = glm::perspective(glm::radians(45.f), (float) width / height, .1f, 1000.f);
     mShaderProgram->Uniform("uProjection", projection);
+
+    Events::GetSingleton()->AddListener("Update", [this](void *) { OnUpdate(); });
+}
+
+void CameraComponent::OnDetach() {
+    Component::OnDetach();
+    Events::GetSingleton()->RemoveListener("Update", listenerId);
 }
 
 std::string CameraComponent::GetType() const {
     return "CameraComponent";
+}
+
+void CameraComponent::OnUpdate() {
+    auto transform = (TransformComponent*) mEntity.lock()->GetComponent("TransformComponent").get();
+    auto view = transform->GetMatrix();
+    auto inverse = glm::inverse(view);
+    mShaderProgram->Uniform("uView", inverse);
 }
 
 CameraComponent::CameraComponent(std::shared_ptr<ShaderProgram> shaderProgram)
