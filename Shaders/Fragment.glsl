77b8d3b2b9ac3b751a918f0c859920216dca61eb
Martin Chuckeles <martin@jakubik.info>
2016-10-31 20:57:12 +0100

    Add fog

--- Shaders/Fragment.glsl
+++ Shaders/Fragment.glsl
@@ -13,10 +13,16 @@ out vec4 oColor;
 
 void main()
 {
+    const float e = 2.71828182845904523536028747135266249;
+    vec4 fogColor = vec4(0.0, 0.0, 0.0, 1.0);
+    float fogDensity = 0.03;
+    float fog = pow(e, -pow(length(EyePosition) * fogDensity, 2));
+
     vec4 textureColor = texture(uTexture, TexCoord) * vec4(Color, 1.0);
 
     float sunDotNormal = clamp(dot(EyeSun, EyeNormal), 0.0, 1.0);
     float light =  0.2 + sunDotNormal * 0.8;
 
-    oColor = vec4(textureColor.xyz * light, 1.0);
+    vec4 finalColor = vec4(textureColor.xyz * light, 1.0);
+    oColor = mix(fogColor, finalColor, fog);
 }
